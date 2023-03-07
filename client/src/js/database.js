73fc5275f36f99a3c9b58f305a892316f13ba5c2
Luis <hairchevere@gmail.com>
2023-03-07 11:07:49 -0600

    added logix to the Put method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,24 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
+  // Create a connection to the database database and version we want to use.
+const contactDb = await openDB('jate', 1);
+
+// Create a new transaction and specify the database and data privileges.
+const tx = contactDb.transaction('jate', 'readwrite');
+
+//open up the desired object store
+const store = tx.objectStore('jate');
+
+// use the .getAll() method to get all data in database.
+const request = store.put({ id: 1, value: content });
+
+const result = await request;
+console.log('result.value', request);
+return result;
+
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+
 
 initdb();
