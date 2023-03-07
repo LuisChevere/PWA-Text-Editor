c03d62a4257ba824ce1a54ee6d2bf9944a65b677
Luis <hairchevere@gmail.com>
2023-03-07 11:37:56 -0600

    added webpack manifest plugin and edited inject manifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,15 +18,33 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'Jate',
       }),
-      new MiniCssExtractPlugin(),
-      new WorkboxPlugin.GenerateSW(),
       new InjectManifest({
         swSrc: './src/sw.js',
-        swDest: 'service-worker.js',
-      })
+        swDest: './src/sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
     ],
+  
 
     module: {
       rules: [
