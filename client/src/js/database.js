426bc7ffd33538c57581bbb982d43742938d9073
Luis <hairchevere@gmail.com>
2023-03-07 13:35:44 -0600

    make it better

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,33 +13,34 @@ const initdb = async () =>
   });
 
 
-export const putDb = async (content) => {console.log('putDb implemented');
+export const putDb = async (content) => {
+  console.log('putDb implemented');
 
   // Create a connection to the database database and version we want to use.
-const contactDb = await openDB('jate', 1);
+const jateDb = await openDB('jate', 1);
 
 // Create a new transaction and specify the database and data privileges.
-const tx = contactDb.transaction('jate', 'readwrite');
+const tx = jateDb.transaction('jate', 'readwrite');
 
 //open up the desired object store
 const store = tx.objectStore('jate');
 
 // use the .put method to add data in database.
-const request = store.put({ id: 1, value: content });
+const request = store.put({ jateDb: content });
 
 const result = await request;
-console.log('🚀 - data saved to the database', request);
-return result;
+console.log('🚀 - data saved to the database', result);
+// return result;
 };
 
 
 export const getDb = async () => {console.log('getDb implemented');
 
 //create a connection to the database database and version we want to use.
-const contactDb = await openDB('jate', 1);
+const jateDb = await openDB('jate', 1);
 
 //create a new transaction abd specify database and data privileges.
-const tx = contactDb.transaction('jate', 'readonly');
+const tx = jateDb.transaction('jate', 'readonly');
 
 //open up the desired object store.
 const store = tx.objectStore('jate');
@@ -49,8 +50,8 @@ const request = store.getAll();
 
 //get confirmation of the request.
 const result = await request;
-console.log('result.value', result);
-return result;
+console.log(result);
+// return result;
 };
 
 
